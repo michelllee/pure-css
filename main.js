11a55babe4e1141ad2c501aff7a069e3ba993009
Michelle <mtsing@gmail.com>
2020-07-02 12:37:51 -0700

    orbit

---
+++ main.js
@@ -0,0 +1,26 @@
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+let controls = new THREE.OrbitControls(camera);
+controls.addEventListener('change', renderer);
+controls.minDistance = 500;
+controls.maxDistance = 1500;
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+var geometry = new THREE.BoxGeometry();
+var material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+var cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.z = 5;
+
+function animate() {
+    requestAnimationFrame(animate);
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    renderer.render(scene, camera);
+}
+animate();
